15227e5369a5f59f593320f2ea70b703828257ec
jixboa <gyesboa@gmail.com>
2025-04-22 17:55:03 +0000

    SERVER ACTION

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const nextConfig = {
+  /* distDir: "build", // Specify your desired output directory here */
+  /*   output: "standalone", */
+  /* output: "export", */
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
+
+/* module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Replace 'crypto' with 'crypto-browserify' and 'stream' with 'stream-browserify' using the fallback property
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    };
+
+    return config;
+  },
+};
+ */
